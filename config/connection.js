f3a472d698d2a67a13d3a2472ce59d16843b1d91
Jorge Baez <jibm_82@yahoo.com.mx>
2018-11-24 20:41:29 -0600

    Updated connection file

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 const mysql = require("mysql");
+let connection;
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  post: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST | "localhost",
+    post: process.env.DB_PORT | 3306,
+    user: process.env.DB_USER | "root",
+    password: process.env.DB_PASSWORD | "root",
+    database: process.env.DB_NAME | "burgers_db"
+  });
+}
 
 connection.connect((err) => {
   if (err) {
